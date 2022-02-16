05bc26bc9664d5d0bc5e1dfed50458571990324b
Carminati Christian <Carminati.17141>
2022-02-16 12:57:05 +0100

    update

--- frontend/src/components/CreateMoviePage.js
+++ frontend/src/components/CreateMoviePage.js
@@ -12,23 +12,68 @@ export default class CreateMoviePage extends Component {
     constructor(props) {
         super(props);
     }
+    handleRoomButtonPressed() 
+    {
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            votes_to_skip: this.state.votesToSkip,
+            guest_can_pause: this.state.guestCanPause,
+          }),
+        };
+        fetch("/api/movies/add", requestOptions)
+          .then((response) => response.json())
+          .then((data) => console.log(data));
+    }
 
     render(){
-        return <Grid container spacing={1}>
-            <Grid items xs={12} align="center">
-                <Typography component="h4" variant="h4">
-                    CreateMoviePage
-                </Typography>
+        return (
+            <Grid container spacing={1}>
+                <Grid items xs={12} align="center">
+                    <Typography component="h4" variant="h4">
+                        CreateMoviePage
+                    </Typography>
+                </Grid>
+                <Grid items xs={12} align="center">
+                    <FormControl component="fieldset">
+                        <TextField required={true} id="standard-basic" label="Name" variant="standard" />
+                            <FormHelperText>
+                                <div>prova</div>
+                            </FormHelperText>
+                        <TextField required={true} id="adwdadwa" label="Movie Director" variant="standard" />
+                        <FormHelperText>
+                            <div align="center">Votes Required To Skip Song</div>
+                        </FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                        label="Basic example"
+                        value={value}
+                        onChange={(newValue) => {
+                        setValue(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                    </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        onClick={this.handleRoomButtonPressed}
+                    >
+                        Create A Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" to="/" component={Link}>
+                        Back
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid items xs={12} align="center">
-                <FormControl>
-                    <FormHelperText>
-                        
-                    </FormHelperText>
-                    <TextField id="standard-basic" label="Name" variant="standard" />
-                </FormControl>
-                
-            </Grid>
-        </Grid>
+        );
     }
 }
